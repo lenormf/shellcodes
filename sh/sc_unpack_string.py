71edd65167828fff2748e2acb1a7919ae7225b76
Frank LENORMAND <lenormf@gmail.com>
2014-05-06 16:49:02 +0200

    Fixed the unpacking helper script

--- sh/sc_unpack_string.py
+++ sh/sc_unpack_string.py
@@ -7,15 +7,38 @@
 import sys
 import struct
 
-def main(ac, av):
-	if (ac < 2):
-		print "Usage: {0} <string> ...".format(av[0])
+def usage(av):
+	print "Usage: {0} <size> <string> ...".format(av)
 
-	for s in av[1:]:
-		s = [ s[i:i + 8] for i, _ in enumerate(s) if i % 8 == 0 ]
+def main(ac, av):
+	size_formats = {
+		8: "Q",
+		4: "L",
+		2: "H",
+		1: "B",
+	}
+
+	if (ac < 3):
+		usage(av[0])
+		return 0
+
+	try:
+		size = int(av[1])
+		str_format = size_formats[size]
+	except ValueError:
+		usage(av[0])
+		return 1
+	except KeyError:
+		print "Invalid size (has to be {0})".format(size_formats.keys())
+		return 2
+
+	for s in av[2:]:
+		s = [ s[i:i + size] for i, _ in enumerate(s) if i % size == 0 ]
 
 		for i in s:
-			print hex(struct.unpack("<Q", i if len(i) == 8 else "".join(["\xFF" for c in xrange(0, 8 - len(i))] + i.split()))[0])
+			padded_substr = "".join(["\xFF" for c in xrange(0, size - len(i))]) + i
+
+			print hex(struct.unpack("@{0}".format(str_format), i if len(i) == size else padded_substr)[0])
 
 if __name__ == "__main__":
 	main(len(sys.argv), sys.argv)
